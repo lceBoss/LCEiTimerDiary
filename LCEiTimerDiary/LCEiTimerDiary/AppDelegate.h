d9f28e2de6366cc1f456bc086cb4d3b2d7e4255b
lceBoss <614695336@qq.com>
2017-09-26 14:48:18 +0800

    创建项目

---
+++ LCEiTimerDiary/LCEiTimerDiary/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  LCEiTimerDiary
+//
+//  Created by 妖狐小子 on 2017/9/26.
+//  Copyright © 2017年 妖狐小子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
